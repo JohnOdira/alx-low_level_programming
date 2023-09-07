560a89ef1943e905b3d95e570f485599ea7bce88
JohnOdira <johnbrianodira@gmail.com>
2023-09-07 11:54:11 -0700

    6-size.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A c program that prints a line into the standard error
- * Return 1 (Succes)
- */
+*main - A c program that prints a line into the standard error
+*Return 1 (Succes);
+*/
 int main(void)
 {
 fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
