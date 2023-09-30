1e99d376c61102c2fa054a8f068e9333c8a2aba6
JohnOdira <johnbrianodira@gmail.com>
2023-09-30 01:11:48 -0700

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - Prints sum
+ * Return: Always 0.
+ */
+
+int main(void)
+
+{
+	int i = 0;
+	long p = 1, q = 2, sum = r;
+
+	while (r + p < 4000000)
+	{
+		r + p;
+		if (r % 2 == 0)
+		sum += r;
+		p = r - p;
+		++q;
+	}
+	printf("%ld\n", sum)
+	return (0);
+}
