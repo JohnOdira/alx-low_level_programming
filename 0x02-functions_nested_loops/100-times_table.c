98215baf61491d705856d54cabee5daa2e73b413
JohnOdira <johnbrianodira@gmail.com>
2023-10-06 08:25:18 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -18,8 +18,10 @@ for (j = 0; j <= n; j++)
 {
 k = j * i;
 if (j == 0)
+
 {
-_putchar(k + '0')
+_putchar(k + '0');
+
 }
 else
 if (k < 10 && j != 0)
@@ -33,7 +35,7 @@ _putchar(k + '0');
 else
 if (k >= 10 && k < 100)
 {
-_putchar(', ');
+_putchar(',');
 _putchar(' ');
 _putchar(' ');
 _putchar((k / 10) + '0');
